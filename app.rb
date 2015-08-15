06761e8b9b8ec0a169fd08958742c8ffcd846ec5
sd-kin <sd-kin@rambler.ru>
2015-08-15 14:50:14 +0300

    cart page

--- app.rb
+++ app.rb
@@ -16,5 +16,27 @@ get '/' do
 end
 
 get '/cart' do
-
+@items = params['cart']
+@total=[]
+@items=@items.gsub('product_',"0").split(',')
+@items.each do |x|
+@total<<x.split("=")
 end
+@total.map!{|x| x.map!{|y| y.to_i}}
+products=Product.all
+@total.each do |x|
+x[0]=products.find(x[0])
+end
+cnt=0
+@tprice=0
+@result=[]
+@total.each do |x|
+cnt+=1
+@tprice+=x[0].price*x[1]
+@result<<{count: cnt, name: x[0].title, number: x[1], price: x[0].price*x[1]}
+end
+
+
+erb :cart
+end
+
