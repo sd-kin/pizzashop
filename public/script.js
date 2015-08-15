585733820f7ac2fbad6b7142fd2216a99f521bec
sd-kin <sd-kin@rambler.ru>
2015-08-15 08:54:10 +0300

    cart button

--- public/script.js
+++ public/script.js
@@ -12,6 +12,8 @@ function addToCart(id) {
 	x=x*1+1;
 	window.localStorage.setItem('product_'+id, x);
 	alert('you add '+x+' items of '+ id);
+	update();
+	button_text();
 }
 
 function showCart() {
@@ -19,12 +21,16 @@ var items = []
 for (var i = 0; i < localStorage.length; i++){
 items.push(localStorage.getItem(localStorage.key(i))+" items", " of id #"+localStorage.key(i));
 }
-alert(items);	
+update();
+alert(items);
+button_text()	
 }
 
 function clearCart() {
 	
 	localStorage.clear();
+	update();
+	button_text()
 }
 
 
@@ -37,7 +43,35 @@ for (var i = 0; i < localStorage.length; i++)
 	var value = window.localStorage.getItem(key);
 	if(key.indexOf('product_') == 0)
 		{
-		cnat = cnt + value*1;
+		cnt = cnt + value*1;
 		}
 	}
+return cnt;
+}
+
+function cart_get_orders()
+{
+var orders='';
+for (var i = 0; i < localStorage.length; i++) 
+	{
+	var key = window.localStorage.key(i);
+	var value = window.localStorage.getItem(key);
+	if(key.indexOf('product_') == 0)
+		{
+		orders = orders + key+'='+value+',';
+		}
+	}
+return orders;
+}
+
+function update()
+{
+var orders = cart_get_orders()
+$('#orders_input').val(orders);
+}
+
+function button_text()
+{
+text = "Cart ("+cart_get_number_of_items()+")"
+$('#cart_button').val(text);
 }
